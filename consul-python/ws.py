29b11f38a35eae4cee75d94f3ebdbdf973dfe5f9
Adriaan de Jonge <adriaandejonge@gmail.com>
2015-04-10 16:34:27 +0530

    Add python file

--- consul-python/ws.py
+++ consul-python/ws.py
@@ -5,23 +5,22 @@ from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 PORT_NUMBER = 8080
 
 class myHandler(BaseHTTPRequestHandler):
-    
+
     #Handler for the GET requests
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
         # Send the html message
-        self.wfile.write("Hello World !")
+        self.wfile.write("Hello World from Docker image!")
         return
 
 try:
     #Create a web server and define the handler to manage the
     #incoming request
     server = HTTPServer(('', PORT_NUMBER), myHandler)
-    print 'Started httpserver on port ' , PORT_NUMBER
-    
-    #Wait forever for incoming htto requests
+
+    #Wait forever for incoming http requests
     server.serve_forever()
 
 except KeyboardInterrupt:
