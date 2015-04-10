aa9e970e4b070562fc4e44307e3d985fd3cb84c7
Adriaan de Jonge <adriaandejonge@gmail.com>
2015-04-10 09:02:29 +0530

    Move out Packer

---
+++ build-all.sh
@@ -0,0 +1,5 @@
+docker build -t xebia/consul base
+docker build -t xebia/consul-env consul-env
+docker build -t xebia/consul-ui consul-ui
+docker build -t xebia/consul-python consul-python
+docker build -t xebia/consul-mongo consul-mongo
