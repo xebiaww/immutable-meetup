0d4b0381d74f31a9d2e0f3815a1d6730640ce2f5
Adriaan de Jonge <adriaandejonge@gmail.com>
2015-08-22 05:15:45 +0000

    Add a xebia/consul-server that starts consul automatically

--- build-all.sh
+++ build-all.sh
@@ -4,3 +4,4 @@ docker build -t xebia/consul-service consul-service
 docker build -t xebia/consul-env consul-env
 docker build -t xebia/consul-ui consul-ui
 docker build -t xebia/consul-python consul-python
+docker build -t xebia/consul-server consul-server
